de7bd31ff100911e6ba46940c733c6901f090038
Timo Hiltel <timo.hiltel@gmail.com>
2021-02-02 15:07:17 +0100

    DynamoDB erstellt und im calc handler gefüllt

--- calc.py
+++ calc.py
@@ -1,9 +1,18 @@
 import json
+from audioop import add
+from decimal import Decimal
+import boto3
+import os
+import time
+from datetime import datetime
+
+dynamodb = boto3.resource('dynamodb')
 
 
 def calculation(event, context):
     nr1 = int(event['pathParameters']['nr1'])
     nr2 = int(event['pathParameters']['nr2'])
+    statuscode = 404
 
     try:
         ressumm = nr1 + nr2
@@ -11,13 +20,15 @@ def calculation(event, context):
         resprod = nr1 * nr2
         resquot = nr1 / nr2
     except ZeroDivisionError:
+        statuscode = 400
         body = {"message": "Zero Division Error"}
         response = {
-            "statusCode": 400,
+            "statusCode": statuscode,
             "body": json.dumps(body)
         }
         return response
 
+    statuscode = 200
     body = {
         "Summe": str(ressumm),
         "Differenz": str(resdiff),
@@ -26,8 +37,28 @@ def calculation(event, context):
     }
 
     response = {
-        "statusCode": 200,
+        "statusCode": statuscode,
         "body": json.dumps(body)
     }
 
+    addToTabel(statuscode, body, ressumm, resdiff, resprod, resquot, nr1, nr2)
     return response
+
+
+
+def addToTabel(statuscode, body, ressumm,resdiff,resprod,resquot, nr1, nr2):
+    table = dynamodb.Table(os.environ['TABLE_NAME'])
+    item = {
+        'timestamp': str(time.time()),
+        'statuscode': str(statuscode),
+        'ressumm': ressumm,
+        'resdiff': resdiff,
+        'resprod': resprod,
+        'resquot': Decimal(resquot),
+        'nr1': nr1,
+        'nr2': nr2,
+    }
+
+    table.put_item(Item=item)
+
+
