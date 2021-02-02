9e4e125b8da91edeaf75a8068f4d5c501916dc03
Timo Hiltel <timo.hiltel@gmail.com>
2021-02-02 11:09:30 +0100

    Hello World - function

--- handler.py
+++ handler.py
@@ -3,8 +3,8 @@ import json
 
 def hello(event, context):
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "message": "Hello World"#,
+       # "input": event
     }
 
     response = {
