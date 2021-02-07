7b38cd3b57a2c6d0e2faa32699e256bf7ef12f23
Timo Hiltel <timo.hiltel@gmail.com>
2021-02-07 19:18:45 +0100

    Neuer Path zum nosql üben

--- nosqlquery.py
+++ nosqlquery.py
@@ -3,21 +3,27 @@ import boto3
 import os
 import decimal
 
-dynamodb = boto3.resource('dynamodb')
+from boto3.dynamodb.conditions import Key, Attr
 
 
-def getall(event, context):
+def getKey(event, context):
+    dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(os.environ['TABLE_NAME'])
 
-    #db.collection.find().limit(1).sort({$natural:-1})
-    # result = table.query(KeyConditionExpression=Key('timestamp').eq('latest_entry_identifier'))
-    result = table.scan()
+    key = event['pathParameters']['key']
+
+    result = table.query(
+        KeyConditionExpression=Key('timestamp').eq(key)
+    )
+
     # create a response
     response = {
         "statusCode": 200,
         "body": json.dumps(result['Items'], cls=DecimalEncoder)
     }
     return response
+
+
 
 #Rückgabe von DynamoDB umwandeln damit JSON die Daten verarbeiten kann
 class DecimalEncoder(json.JSONEncoder):
